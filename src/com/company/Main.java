c7c5aa0038e8c07b2091d32e3f2297f88fc413c8
jrmensah <jerilynmensah@gmail.com>
2017-10-11 13:58:50 -0400

    Printing a receipt

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner keyboard = new Scanner(System.in);
+
+            System.out.println("+--------------------------------+");
+            System.out.println("|                                |");
+            System.out.println("|   Java Bank Receipt            |");
+            System.out.println("|   Wednesday, December 2, 2015  |");
+            System.out.println("|   ATM Location # 123           |");
+            System.out.println("|                                |");
+            System.out.println("|                                |");
+            System.out.println("|                                |");
+            System.out.println("|   Account Number: 1234567      |");
+            System.out.println("|   Customer: John Q. Public     |");
+            System.out.println("|   Transaction Type: Deposit    |");
+            System.out.println("|   Transaction Amount: $500.00  |");
+            System.out.println("|   Account Balance: $1000.00    |");
+            System.out.println("|                                |");
+            System.out.println("|   Thank you for banking with us|");
+            System.out.println("|   Have a coffee day            |");
+            System.out.println("|                                |");
+            System.out.println("+--------------------------------+");
+    }
+}
